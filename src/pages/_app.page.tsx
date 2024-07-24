6a22a4bc05fef411f6b39badebcc88a89bd90b1f
wo-o29 <woogur29@gmail.com>
2024-07-24 23:12:29 +0900

    chore: tanstack query 초기 설정

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,25 @@
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 0, // 재시도 횟수
+            staleTime: 1 * 60 * 1000, // 1분
+            gcTime: 5 * 60 * 1000, // 5분
+            throwOnError: true, // 에러 발생 시 컴포넌트에 에러를 전파
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
