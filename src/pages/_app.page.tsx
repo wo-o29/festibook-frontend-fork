80f5b13937c8edd1111c7b6e1b0ee299cbc846cd
wo-o29 <woogur29@gmail.com>
2024-07-25 14:06:30 +0900

    chore: import 순서 초기 설정

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,8 @@
-import "../styles/global";
 import type { AppProps } from "next/app";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import "@/styles/global";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
