3bf29c95143dadfc4cae6cf51afe116181a9eda8
wo-o29 <woogur29@gmail.com>
2024-08-02 18:18:37 +0900

    refactor: 레이아웃 main 태그 제거

--- src/components/Layout/HeaderLayout/index.tsx
+++ src/components/Layout/HeaderLayout/index.tsx
@@ -8,7 +8,7 @@ function HeaderLayout({ children }: HeaderLayoutProps) {
   return (
     <>
       <header>추후 헤더 완성되면 만들어진 헤더 사용 예정 </header>
-      <main>{children}</main>
+      {children}
     </>
   );
 }
