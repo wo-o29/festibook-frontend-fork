d370d9d770bffdd6aca4f170437595ffdaafdfe8
wo-o29 <woogur29@gmail.com>
2024-08-30 20:29:35 +0900

    refactor: 악시오스 인스턴스 로직 수정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,11 +1,13 @@
 import { GuideBookType } from "@/types";
 
-import { guideBookInstance } from "./axios";
+import { setInstance } from "./axios";
 
 export const getGuideBookData = async (
   pageParam: number,
 ): Promise<GuideBookType> => {
-  const response = await guideBookInstance.get(
+  const response = await setInstance(
+    process.env.NEXT_PUBLIC_GUIDE_BOOK_API_BASE_URL!,
+  ).get(
     `/15123631/v1/uddi:33264f0a-158f-4a5d-95cd-99c740c8a097?page=${pageParam}&perPage=20&serviceKey=${process.env.NEXT_PUBLIC_DATA_GO_API_KEY}`,
   );
   return response.data;
--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,18 +1,13 @@
-import axios from "axios";
-
-export const guideBookInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_GUIDE_BOOK_API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+import axios, { AxiosInstance } from "axios";
 
 export const instance = axios.create({
   baseURL: "baseURL 추가 예정",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
+
+export const setInstance = (baseUrl: string): AxiosInstance => {
+  instance.defaults.baseURL = baseUrl;
+  return instance;
+};
 
 // 헤더 토큰 추가할 때 사용 등등...
 instance.interceptors.request.use();
