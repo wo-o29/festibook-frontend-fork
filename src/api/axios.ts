1ca01f0ccf470d8f2c807c59b5ced032e3c7b95b
wo-o29 <woogur29@gmail.com>
2024-09-09 17:47:17 +0900

    feat: baseUrl 설정(로컬)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "baseURL 추가 예정",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "http://localhost:8080",
 });
 
 // 헤더 토큰 추가할 때 사용 등등...
