3579d4e15e90c2b4ffa7c5953408ac907d0f2a8f
wo-o29 <woogur29@gmail.com>
2024-09-29 17:04:21 +0900

    feat: 미들웨어 추가

--- src/middleware.page.ts
+++ src/middleware.page.ts
@@ -1,1 +1,16 @@
-export function middleware() {}
+import { NextResponse, type NextRequest } from "next/server";
+
+const userOnlyUrlRegex = /^(\/mypage)/;
+
+export function middleware(request: NextRequest) {
+  const accessToken = request.cookies.get("accessToken");
+  const pathName = request.nextUrl.pathname;
+
+  if (!accessToken && userOnlyUrlRegex.test(pathName)) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|images|svgs|_next/static|_next/image|favicon.ico).*)"],
+};
